fe1963c00d51fe6d9b97d341f86509149ea8134f
Louis <louis.paret@gmail.com>
2022-06-03 13:57:43 +0700

    Fix comment

--- env-file/source-dot-env-file.sh
+++ env-file/source-dot-env-file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the file .env exists, if not it stops the script
-# The check is bypassed if we are on Jenkins
+# The check is bypassed if we are on Jenkins or Gitlab
 if [[ ! -f .env && ! ${JENKINS_HOME} && ! ${GITLAB_CI} ]]; then
     printf 'Please create the file .env from the template .env_template and by adding your credentials.\n' >&2
     exit 1
