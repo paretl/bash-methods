205aec114f319914c824373df815ccad68589bc9
Louis <louis.paret@gmail.com>
2022-12-26 21:49:39 +0100

    Add Github Actions verification in env-file method

--- env-file/source-dot-env-file.sh
+++ env-file/source-dot-env-file.sh
@@ -2,7 +2,7 @@
 
 # Check if the file .env exists, if not it stops the script
 # The check is bypassed if we are on Jenkins or Gitlab
-if [[ ! -f .env && ! ${JENKINS_HOME} && ! ${GITLAB_CI} ]]; then
+if [[ ! -f .env && ! ${JENKINS_HOME} && ! ${GITLAB_CI} $$ ! ${GITHUB_ACTIONS} ]]; then
     printf 'Please create the file .env from the template .env_template and by adding your credentials.\n' >&2
     exit 1
 fi
